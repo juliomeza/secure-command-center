3bbdfe1e2a6f604eb75e9f68a863af11c6eb9f8a
Julio Meza <juliomeza@gmail.com>
2025-04-07 16:41:35 -0400

    Text no se necesita importar

--- frontend/src/components/charts/PieChart.tsx
+++ frontend/src/components/charts/PieChart.tsx
@@ -6,8 +6,7 @@ import {
   Cell,
   Tooltip,
   ResponsiveContainer,
-  TooltipProps,
-  Text
+  TooltipProps
 } from 'recharts';
 import { DataPoint } from '../../data/types'; // Assuming DataPoint has 'name', 'value', 'color'
 
