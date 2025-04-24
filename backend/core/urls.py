1717d6c3b5e8643e73e51a21aa84d1b953c0223a
Julio Meza <juliomeza@gmail.com>
2025-04-24 16:02:44 -0400

    Auth - Iniciando Proceso de eliminacion de codigo antiguo

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,13 +1,5 @@
 # backend/core/urls.py
 from django.urls import path
-from . import views
-from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path('profile/', views.UserProfileView.as_view(), name='user-profile'),
-    path('token/', views.TokenObtainView.as_view(), name='token-obtain'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
-    path('csrf/', views.get_csrf_token, name='csrf-token'),
-    path('oauth-success/', views.oauth_success_redirect, name='oauth-success'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
