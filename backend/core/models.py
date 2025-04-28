a8affe8d9dbd08c4049bd363882c3360f8e857ca
Julio Meza <juliomeza@gmail.com>
2025-04-28 11:13:36 -0400

    Removi el core app

--- backend/core/models.py
+++
@@ -1,5 +0,0 @@
-# backend/core/models.py
-from django.db import models
-from django.contrib.auth.models import User
-
-# UserProfile model has been moved to the authentication app
--- backend/project/settings.py
+++ backend/project/settings.py
@@ -103,7 +103,6 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',  # JWT token blacklist
 
     # Your apps
-    'core',
     'authentication',
     'access',
 ]
