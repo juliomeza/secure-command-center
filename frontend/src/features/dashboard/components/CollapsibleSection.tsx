5dfddd4be588ec4bb55d0b73047da297ba506263
Julio Meza <juliomeza@gmail.com>
2025-05-06 08:11:56 -0400

    Arreglando un issue menor

--- frontend/src/features/dashboard/components/CollapsibleSection.tsx
+++ frontend/src/features/dashboard/components/CollapsibleSection.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ChevronDown, Package, TruckIcon, ClipboardList, AlertCircle } from 'lucide-react';
+import { ChevronDown, TruckIcon, ClipboardList, AlertCircle } from 'lucide-react';
 import Card from '../cards/Card';
 
 interface CollapsibleSectionProps {
