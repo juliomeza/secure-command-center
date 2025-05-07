8e9fd16d0879ed3f8ddf0306ffc57cd103515218
Julio Meza <juliomeza@gmail.com>
2025-05-07 10:37:53 -0400

    Borde de los filtros mejroados

--- frontend/src/features/dashboard/components/CustomSelect.tsx
+++ frontend/src/features/dashboard/components/CustomSelect.tsx
@@ -43,9 +43,10 @@ const CustomSelect: React.FC<CustomSelectProps> = ({
       <div className="w-full">
         <button
           onClick={() => setIsOpen(!isOpen)}
-          className="flex items-center w-full px-3 py-2 border border-blue-400 rounded-lg bg-white focus:outline-none gap-2"
+          className="flex items-center w-full px-3 py-2 rounded-lg bg-white focus:outline-none gap-2"
           type="button"
           style={{
+            border: '1px solid #1e40af',
             fontSize: '0.95rem',
             color: '#1e293b',
             cursor: 'pointer',
